74d811d803c5873819d8d1b88d298b4a0d58bcb5
JeffersonGonzalezC <85133052+JeffersonGonzalezC@users.noreply.github.com>
2021-06-11 17:23:04 -0600

    DSF

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My first react deploy!!
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
